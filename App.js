961cb10969f81f1a0ed80eec5a17951386d59d29
paweltar <paweltar@gmail.com>
2017-08-12 16:52:56 +0200

    Flexbox

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import { AppRegistry, View, StyleSheet } from 'react-native';
 
+import Toggle from './Toggle';
+
 export default class App extends Component {
-  render () {
+
+  state = {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+  render() {
+    const { flexDirection, alignItems, justifyContent } = this.state;
+    const layoutStyle = { flexDirection, justifyContent, alignItems };
+
+    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical';
+    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal';
+
     return (
       <View style={styles.container}>
-        <View style={styles.box}>
-          <View style={styles.innerBox}/>
+        <Toggle
+          label={'Primary axis (flexDirection)'}
+          value={flexDirection}
+          options={['row', 'column']}
+          onChange={(option) => this.setState({flexDirection: option})}
+        />
+        <Toggle
+          label={primaryAxis + ' distribution (justifyContent)'}
+          value={justifyContent}
+          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
+          onChange={(option) => this.setState({justifyContent: option})}
+        />
+        <Toggle
+          label={secondaryAxis + ' alignment (alignItems)'}
+          value={alignItems}
+          options={['flex-start', 'center', 'flex-end', 'stretch']}
+          onChange={(option) => this.setState({alignItems: option})}
+        />
+        <View style={[styles.layout, layoutStyle]}>
+          <View style={styles.box} />
+          <View style={styles.box} />
+          <View style={styles.box} />
         </View>
       </View>
     );
@@ -16,24 +51,16 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  },
+  layout: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.05)',
   },
   box: {
-    width: 200,
-    height: 200,
-    backgroundColor: 'skyblue',
-    borderWidth: 2,
-    borderColor: 'steelblue',
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    padding: 25,
+    backgroundColor: 'steelblue',
+    margin: 5,
   },
-  innerBox: {
-    width: 20,
-    height: 20,
-    backgroundColor: 'red'
-  }
-});
+})
 
 AppRegistry.registerComponent('App', () => App);
-
